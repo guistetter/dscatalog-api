c5dfe6a57ab18dc6088b4767613f647dbee7deb5
guistetter <guilherme_stetter@hotmail.com>
2023-07-16 18:34:41 -0300

    DTO created

--- backend/dscatalog/src/main/java/com/dev/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/dev/dscatalog/services/CategoryService.java
@@ -1,23 +1,27 @@
 package com.dev.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dev.dscatalog.dto.CategoryDTO;
 import com.dev.dscatalog.entities.Category;
 import com.dev.dscatalog.repositories.CategoryRepository;
 
-@Service 
+@Service
 public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
-	
-	//Evita o locking do banco pois é uma operação apenas de leitura
+
+	// ReadOnlyEvita o locking do banco pois é uma operação apenas de leitura
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
-		
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
 	}
 }
