0260c7c75d9d9ddb96fea69cc1e954120b0d17f3
guistetter <guilherme_stetter@hotmail.com>
2023-07-16 17:59:31 -0300

    data seeding

--- backend/dscatalog/src/main/java/com/dev/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/dev/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dev.dscatalog.entities.Category;
 import com.dev.dscatalog.repositories.CategoryRepository;
@@ -13,6 +14,8 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	//Evita o locking do banco pois é uma operação apenas de leitura
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
 		
